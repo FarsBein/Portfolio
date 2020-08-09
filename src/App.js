e6ec5e4f53c4a6e4949525d297728589c4167e62
Fars Bein <53714581+FarsBein@users.noreply.github.com>
2020-08-09 13:35:34 -0400

    add gh-pages

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import * as ReactBootStrap from "react-bootstrap";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import FirstSlide from './Component/HomePage/FirstSlide'
-import Projects from './Component/unused/Projects'
 import Navbar from './Component/Navbar'
 import Footer from './Component/HomePage/Footer';
 import Resume from './Component/Resume/Resume';
