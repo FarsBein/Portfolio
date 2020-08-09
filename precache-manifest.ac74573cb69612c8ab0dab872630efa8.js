66f38a0ed47a5b9c43a49be7bf74140e05aa6f12
Fars Bein <53714581+FarsBein@users.noreply.github.com>
2020-08-09 13:49:40 -0400

    Updates

---
+++ precache-manifest.ac74573cb69612c8ab0dab872630efa8.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b27034edb62a455b3124182ad08ecfdf",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "8873796de9089520106f",
+    "url": "/Portfolio/static/css/2.9b79e585.chunk.css"
+  },
+  {
+    "revision": "5394858dd924273dfc6a",
+    "url": "/Portfolio/static/css/main.60fc01a3.chunk.css"
+  },
+  {
+    "revision": "8873796de9089520106f",
+    "url": "/Portfolio/static/js/2.b7b8c555.chunk.js"
+  },
+  {
+    "revision": "9a2cf355ca348d84eedb0129142dc576",
+    "url": "/Portfolio/static/js/2.b7b8c555.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5394858dd924273dfc6a",
+    "url": "/Portfolio/static/js/main.2df53f46.chunk.js"
+  },
+  {
+    "revision": "8a7ad5256a3da5a04187",
+    "url": "/Portfolio/static/js/runtime-main.cffb452f.js"
+  },
+  {
+    "revision": "a14f4c6c44e1a727a5288f5c50779d91",
+    "url": "/Portfolio/static/media/Meme.a14f4c6c.JPG"
+  },
+  {
+    "revision": "b79fe94b0b899a6e56185ce8a1538db5",
+    "url": "/Portfolio/static/media/chess_1.b79fe94b.jpg"
+  },
+  {
+    "revision": "ccde188d6bb2f2b487c153ffd00f6b61",
+    "url": "/Portfolio/static/media/maze_gif.ccde188d.gif"
+  },
+  {
+    "revision": "69d4262558168e46e32668a4c5eb8621",
+    "url": "/Portfolio/static/media/project_1.69d42625.jpg"
+  },
+  {
+    "revision": "721ee2c7de9b79dc317085f27e0229b5",
+    "url": "/Portfolio/static/media/reddit_wallpaper.721ee2c7.jpg"
+  },
+  {
+    "revision": "debb0a0f6ffbea0b9473739ba7da177d",
+    "url": "/Portfolio/static/media/resume_web-1.debb0a0f.jpg"
+  },
+  {
+    "revision": "495a94ae853e1bb9f4bdb147b5db3dea",
+    "url": "/Portfolio/static/media/resume_web.495a94ae.pdf"
+  },
+  {
+    "revision": "f619bd15fef5246dd0cdc52b0a0d6215",
+    "url": "/Portfolio/static/media/snake_gif.f619bd15.gif"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/snake-js/precache-manifest.c22347afb25221ddd71d90bfc755c9b6.js"
+  "/Portfolio/precache-manifest.ac74573cb69612c8ab0dab872630efa8.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/snake-js/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
