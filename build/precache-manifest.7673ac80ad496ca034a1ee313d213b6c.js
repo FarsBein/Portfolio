1fe3103f4e495d3e2679ee84afe9f116c093eb63
Fars Bein <53714581+FarsBein@users.noreply.github.com>
2020-08-10 19:33:15 -0400

    add .gitignore

---
+++ build/precache-manifest.7673ac80ad496ca034a1ee313d213b6c.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3867106d7a5f69c1e8495c01180fc8fa",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "8873796de9089520106f",
+    "url": "/portfolio/static/css/2.9b79e585.chunk.css"
+  },
+  {
+    "revision": "5394858dd924273dfc6a",
+    "url": "/portfolio/static/css/main.60fc01a3.chunk.css"
+  },
+  {
+    "revision": "8873796de9089520106f",
+    "url": "/portfolio/static/js/2.b7b8c555.chunk.js"
+  },
+  {
+    "revision": "9a2cf355ca348d84eedb0129142dc576",
+    "url": "/portfolio/static/js/2.b7b8c555.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5394858dd924273dfc6a",
+    "url": "/portfolio/static/js/main.2df53f46.chunk.js"
+  },
+  {
+    "revision": "f89f2099577e680dc7b6",
+    "url": "/portfolio/static/js/runtime-main.70964fd7.js"
+  },
+  {
+    "revision": "a14f4c6c44e1a727a5288f5c50779d91",
+    "url": "/portfolio/static/media/Meme.a14f4c6c.JPG"
+  },
+  {
+    "revision": "b79fe94b0b899a6e56185ce8a1538db5",
+    "url": "/portfolio/static/media/chess_1.b79fe94b.jpg"
+  },
+  {
+    "revision": "ccde188d6bb2f2b487c153ffd00f6b61",
+    "url": "/portfolio/static/media/maze_gif.ccde188d.gif"
+  },
+  {
+    "revision": "69d4262558168e46e32668a4c5eb8621",
+    "url": "/portfolio/static/media/project_1.69d42625.jpg"
+  },
+  {
+    "revision": "721ee2c7de9b79dc317085f27e0229b5",
+    "url": "/portfolio/static/media/reddit_wallpaper.721ee2c7.jpg"
+  },
+  {
+    "revision": "debb0a0f6ffbea0b9473739ba7da177d",
+    "url": "/portfolio/static/media/resume_web-1.debb0a0f.jpg"
+  },
+  {
+    "revision": "495a94ae853e1bb9f4bdb147b5db3dea",
+    "url": "/portfolio/static/media/resume_web.495a94ae.pdf"
+  },
+  {
+    "revision": "f619bd15fef5246dd0cdc52b0a0d6215",
+    "url": "/portfolio/static/media/snake_gif.f619bd15.gif"
+  }
+]);
