260b4ef380b909b29404ad5a59ce38a104a3a74d
Fars Bein <53714581+FarsBein@users.noreply.github.com>
2021-01-18 17:07:12 -0500

    added image for teamup hub project

--- src/Data/projectsList.js
+++ src/Data/projectsList.js
@@ -4,8 +4,25 @@ import Meme from '../img/Meme.JPG'
 import snake_gif from '../img/snake_gif.gif'
 import reddit_wallpaper from '../img/reddit_wallpaper.jpg'
 import project_1 from '../img/project_1.jpg'
+import TeemUp_hub from '../img/teamup2.gif'
 
 export default [
+    {
+        title:'Coronavirus Information Center',
+        points: ['⛏️ Fetches and displays reported cases and covid related news from an open API','🖥️ Includes Testing locations in the Ontario area', '✍️ A Feedback page that uploads comments to MySQL database'],
+        text:'Collaborated with a team to build a one-stop website for all coronavirus related information in order to raise awareness and provide resources for people that are seeking accurate live data',
+        tools:['Javascript', 'HTML', 'CSS', 'MySQL', 'PHP', 'API'],
+        image: reddit_wallpaper,
+        github:'https://github.com/FarsBein/Coronavirus-Information-Centre'
+    },
+    {
+        title:'TeemUp Hub',
+        points: ['🔐 Registration and authentication functionalities','💬 Chat rooms and past conversation page','✍️ Can create and share new posts'],
+        text:'online advertising service that operates as a centralized network of online communities for posting cooperation requests',
+        tools:['React','MongoDB', 'Express','Node','Javascript','CSS'],
+        image: TeemUp_hub,
+        github:'https://github.com/FarsBein/Teemup-hub'
+    },
     {
         title:'Maze + A*',
         points: ['🧩 Generators random mazes','🖥️ Uses pygame to display and interact with the maze','🖱️ Can draw (left-click) and erase (right-click) on the maze','⌨️ Pressing Space will automatically solve the maze using A* algorithm'],
@@ -28,23 +45,15 @@ export default [
         text:'A two-player strategy board game with 64 squares arranged in an 8×8 grid. UI with no chess rules logic',
         tools:['ReactJs','Javascript','CSS'],
         image: chess_1,
-        github:'https://github.com/FarsBein/Maze_A_Star'
+        github:'https://github.com/FarsBein/react-chess-game'
     },
     {
         title:'Random Meme Maker',
         points: ['⚛️ Built using react classes','🖼️ Fetches random funny images from memeAPI','📱 Mobile friendly'],
         text:'Meme maker that fitches random images from memeAPI. Images can be customized by captioning them with top and bottom texts',
-        tools:['ReactJs','Javascript'],
+        tools:['ReactJs','Javascript', 'API'],
         image: Meme,
-        github:'https://github.com/FarsBein/Maze_A_Star'
-    },
-    {
-        title:'TeemUp Hub',
-        points: ['🔐 Registration and authentication functionalities','💬 Chat rooms and past conversation page','✍️ Can create and share new posts'],
-        text:'online advertising service that operates as a centralized network of online communities for posting cooperation requests',
-        tools:['ReactJs','Javascript','CSS'],
-        image: project_1,
-        github:'https://github.com/FarsBein/Maze_A_Star'
+        github:'https://github.com/FarsBein/meme-maker'
     },
     {
         title:'Reddit Api wallpaper',
@@ -52,6 +61,6 @@ export default [
         text:'A simple script that changes the desktop wallpaper  to the hottest image on either wallpaper or wallpapers subreddit. Currently it only works on widows',
         tools:['Python'],
         image: reddit_wallpaper,
-        github:'https://github.com/FarsBein/Maze_A_Star'
+        github:'https://github.com/FarsBein/Reddit-Api-wallpaper'
     }
 ]
